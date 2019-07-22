6d0cedb5c1e46c5c85f42504767c085051bea084
pushpa-ramachandran <pushpalatharam.sekar@gmail.com>
2019-07-21 17:27:32 -0700

    Add files via upload

--- CharNeverRepeated.py
+++ CharNeverRepeated.py
@@ -1,8 +1,9 @@
 # Find the char that is not repeated in a string. if more than, return the first repeated char
 
 # if empty or too few return -1
-# one by one, check the charcaters in dictionary. If found, return the char. Else add the char to dictionary
-# if iteration completes, then it means that there is no repeat char. return None
+# one by one, check the charcaters in dictionary. If found, add the count. Else add the char to dictionary
+# Now iterrate thru the dictionary. Return the first char with count == 1
+# if iteration completes, then it means that there is no unique char. return None
 
 def findRepeatChar(str):
     # if empty or too few return -1
@@ -13,28 +14,36 @@ def findRepeatChar(str):
     for ch in str.lower():
         if ch != " ":
             if ch in charDictionary:
-                return ch
+                charDictionary[ch] += 1
             else:
                 charDictionary[ch] = 1
+
+    for c in str.lower():
+        if c != " " and charDictionary[c] == 1:
+            return c
 
     return "None"
 
 # Scenarios
 # with one repeated char
-print('ab cd   ed a- exp a')
+print('ab cd   e a- exp b')
 print(findRepeatChar('ab cd   e a'))
 
 # with multiple repeated chars
-print('ab cd   ed  c b a -exp c')
-print(findRepeatChar('ab cd   e  c b a'))
+print('ab cd   ed  c b a -exp e')
+print(findRepeatChar('ab cd   ed  c b a'))
 
 # with lower and upper repeated chars
-print('ab cd   e  E b a -exp e')
+print('ab cd   e  E b a -exp c')
 print(findRepeatChar('ab cd   e  E b a'))
 
 # with no repeated char
-print('ab cd   ed - exp None')
+print('ab cd   e - exp a')
 print(findRepeatChar('ab cd   e'))
+
+# with all repeated char
+print('ab ab - exp None')
+print(findRepeatChar('ab ab '))
 
 
 # with empty str
